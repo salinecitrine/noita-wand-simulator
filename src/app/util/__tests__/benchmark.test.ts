a39d186bb2e48ed753a82828a8106fd3c7e4af13
Saline Citrine <salinecitrine@gmail.com>
2023-05-15 11:34:31 -0700

    benchmark WIP

--- src/app/util/__tests__/benchmark.test.ts
+++ src/app/util/__tests__/benchmark.test.ts
@@ -19,7 +19,7 @@ const runBenchmark = <T extends any[], R>(
   console.log(`${expect.getState().currentTestName}: ${averageTime}ms`);
 };
 
-describe.skip('combineGroups', () => {
+describe('combineGroups', () => {
   it('d10-d10-d4-d2-d2-a', () => {
     const g1 = ['a', 'a'];
     const g2 = ['d2', ...g1].flat();
@@ -59,6 +59,78 @@ describe.skip('combineGroups', () => {
       },
     ];
     runBenchmark(combineGroups, [input], expected, 10);
+  });
+
+  it('2x d10-d10-d4-d2-d2-a with break', () => {
+    const g1 = ['a', 'a'];
+    const g2 = ['d2', ...g1].flat();
+    const g3 = ['d2', ...Array(2).fill(g2)].flat();
+    const g4 = ['d4', ...Array(4).fill(g3)].flat();
+    const g5 = ['d10', ...Array(10).fill(g4)].flat();
+    const g6 = ['d10', ...Array(10).fill(g5)].flat();
+    const input = [...g6, 'b', ...g6];
+    const expected = [
+      'd10',
+      {
+        count: 10,
+        first: [
+          'd10',
+          {
+            count: 10,
+            first: [
+              'd4',
+              {
+                count: 4,
+                first: [
+                  'd2',
+                  {
+                    count: 2,
+                    first: [
+                      'd2',
+                      {
+                        count: 2,
+                        first: 'a',
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      'b',
+      'd10',
+      {
+        count: 10,
+        first: [
+          'd10',
+          {
+            count: 10,
+            first: [
+              'd4',
+              {
+                count: 4,
+                first: [
+                  'd2',
+                  {
+                    count: 2,
+                    first: [
+                      'd2',
+                      {
+                        count: 2,
+                        first: 'a',
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ];
+    runBenchmark(combineGroups, [input], expected, 1);
   });
 
   it('4000x a', () => {
